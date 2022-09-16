7eb5b8977c5050514027b8febb0124a66a53708d
fesmofet <borees2@gmail.com>
2022-09-16 13:58:36 +0300

    add reCalcCampaignsRewardInUsd

--- src/domain/jobs/jobs.service.ts
+++ src/domain/jobs/jobs.service.ts
@@ -3,7 +3,10 @@ import { Inject, Injectable } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { CAMPAIGN_PROVIDE, SPONSORS_BOT_PROVIDE } from '../../common/constants';
 
-import { CampaignSuspendInterface } from '../campaign/interface';
+import {
+  CampaignHelperInterface,
+  CampaignSuspendInterface,
+} from '../campaign/interface';
 import { SponsorsBotInterface } from '../sponsors-bot/interface';
 
 @Injectable()
@@ -13,6 +16,8 @@ export class JobsService {
     private readonly campaignSuspend: CampaignSuspendInterface,
     @Inject(SPONSORS_BOT_PROVIDE.BOT)
     private readonly sponsorsBot: SponsorsBotInterface,
+    @Inject(CAMPAIGN_PROVIDE.CAMPAIGN_HELPER)
+    private readonly campaignHelper: CampaignHelperInterface,
   ) {}
 
   @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
@@ -23,5 +28,10 @@ export class JobsService {
   @Cron(CronExpression.EVERY_30_MINUTES)
   async matchBotJob(): Promise<void> {
     return this.sponsorsBot.executeUpvotes();
+  }
+
+  @Cron(CronExpression.EVERY_DAY_AT_6PM)
+  async updateRewardJob(): Promise<void> {
+    return this.campaignHelper.reCalcCampaignsRewardInUsd();
   }
 }
