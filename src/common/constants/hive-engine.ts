9c74694aa7e690dbbe88bfaac256dd8b9edef112
fesmofet <borees2@gmail.com>
2023-11-27 17:32:28 +0200

    engine nodes

--- src/common/constants/hive-engine.ts
+++ src/common/constants/hive-engine.ts
@@ -1,11 +1,9 @@
 export const HIVE_ENGINE_NODES = [
- // 'https://api2.hive-engine.com/rpc',
   'https://herpc.dtools.dev',
-  'https://engine.rishipanthee.com',
   'https://api.primersion.com',
   'https://herpc.kanibot.com',
-  'https://api.hive-engine.com/rpc',
-  'https://ha.herpc.dtools.dev',
+  'https://engine.deathwing.me',
+  'https://he.sourov.dev',
 ];
 
 export const TOKEN_WAIV = Object.freeze({
