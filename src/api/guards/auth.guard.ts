c5ca777475c899a3f869237f184ab23fb28e2a3e
fesmofet <borees2@gmail.com>
2024-01-26 10:25:35 +0200

    auth

--- src/api/guards/auth.guard.ts
+++ src/api/guards/auth.guard.ts
@@ -24,7 +24,7 @@ export class AuthGuard implements CanActivate {
     const hiveAuth = headers['hive-auth'];
 
     if (hiveAuth) {
-      return this.validateHiveAuth(account, hiveAuth);
+      return this.validateHiveAuth(account, token);
     }
 
     if (headers['waivio-auth']) {
