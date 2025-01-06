6cd781d12951e7d473ee27687ae27923b388cd7e
fesmofet <borees2@gmail.com>
2025-01-06 10:35:59 +0200

    change nodes

--- src/common/constants/hive-engine.ts
+++ src/common/constants/hive-engine.ts
@@ -1,9 +1,10 @@
 export const HIVE_ENGINE_NODES = [
-  'https://he.sourov.dev',
+  // 'https://he.sourov.dev',
   //  'https://ha.herpc.dtools.dev',
   'https://herpc.actifit.io',
   'https://api.primersion.com',
-  'https://herpc.dtools.dev',
+  'https://api.hive-engine.com/rpc/',
+  // 'https://herpc.dtools.dev',
 ];
 
 export const TOKEN_WAIV = Object.freeze({
